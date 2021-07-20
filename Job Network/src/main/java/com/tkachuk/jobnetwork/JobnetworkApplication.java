e0b4cc4db16db3688b4351231dcda14abd1a4e38
Svitlana Tkachuk <misstkachuk15@gmail.com>
2021-07-20 13:01:14 +0300

    change project structure

---
+++ Job Network/src/main/java/com/tkachuk/jobnetwork/JobnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.tkachuk.jobnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobnetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobnetworkApplication.class, args);
+    }
+
+}
---
+++ Job Network/src/test/java/com/tkachuk/jobnetwork/JobnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkachuk.jobnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobnetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
