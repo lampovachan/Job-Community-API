24c792dcddd3e78b495c35e66c61f006c3b1ac15
Svitlana Tkachuk <misstkachuk15@gmail.com>
2021-07-26 15:41:12 +0300

    add docker

--- run_script.sh
+++ run_script.sh
@@ -1,3 +1,3 @@
 docker-compose up -d &&
-java -jar cvgenerator/target/mywebserviceapp-0.0.1-SNAPSHOT.jar &&
-java -jar jobnetwork/targer/mywebserviceapp-0.0.1-SNAPSHOT.jar
+java -jar cvgenerator/target/cvgenerator-1.jar &&
+java -jar jobnetwork/targer/jobnetwork-0.0.1-SNAPSHOT.jar
